caaba693998487e51d1c9ab423e854271d2837be
SimonChaw <chawblah@gmail.com>
2017-11-16 16:13:33 -0400

    Included body-parser

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 module.exports = app;
 
@@ -12,14 +13,14 @@ const PORT = process.env.PORT || 4001;
 
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
 
 
 // This conditional is here for testing purposes:
-if (!module.parent) { 
+if (!module.parent) {
   // Add your code to start the server listening at PORT below:
 
 }
