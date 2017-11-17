7761f0532e8dd8bcbfbc8558a1809f2bb7e287a5
SimonChaw <chawblah@gmail.com>
2017-11-17 10:34:55 -0400

    Revert "Middleware"

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,12 +1,4 @@
-const checkMillionDollarIdea = (req, res, next) => {
-  const idea = req && req.body;
-  if((Number(idea.numWeeks) * Number(idea.weeklyRevenue)) >= 1000000){
-    console.log((Number(idea.numWeeks) * Number(idea.weeklyRevenue)));
-    next();
-  }else{
-    res.status(400).send('Idea not created');
-  }
-};
+const checkMillionDollarIdea = () => {};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
