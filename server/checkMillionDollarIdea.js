71ed7526c603dd5d5c1de6ca276fc2d5eb947c3e
SimonChaw <chawblah@gmail.com>
2017-11-17 10:43:30 -0400

    Remove console.log

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,7 +1,6 @@
 const checkMillionDollarIdea = (req, res, next) => {
   const idea = req && req.body;
   if((Number(idea.numWeeks) * Number(idea.weeklyRevenue)) >= 1000000){
-    console.log((Number(idea.numWeeks) * Number(idea.weeklyRevenue)));
     next();
   }else{
     res.status(400).send('Idea not created');
